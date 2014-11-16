06bcdd527e3ac2eedec1a6dd5b2eb16fb6c78d74
Chris Sprague <christopherssprague@gmail.com>
2014-11-16 12:41:07 -0500

    Fix final build errors + update gitignore

--- mobile/src/main/java/com/grc/activities/CreateQuestionActivity.java
+++ mobile/src/main/java/com/grc/activities/CreateQuestionActivity.java
@@ -137,9 +137,9 @@ public class CreateQuestionActivity extends Activity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+        //if (id == R.id.action_settings) {
+        //    return true;
+        //}
 
         return super.onOptionsItemSelected(item);
     }
